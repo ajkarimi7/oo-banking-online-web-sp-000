ad0b14933f82b56745d1ffb20654a2b019d99988
Ali Karimi <ajkarimi7@gmail.com>
2019-10-01 20:00:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,4 +7,23 @@ class Transfer
   def initialize(sender, receiver, amount)
     @status = "pending"
     @sender = sender 
+    @receiver = receiver
+    @amount = amount
+  end 
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end 
+  
+  def execute_transaction
+    if valid? && sender.balance > && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else 
+      reject_transfer
+    end
+  end 
+  
+  
 end
