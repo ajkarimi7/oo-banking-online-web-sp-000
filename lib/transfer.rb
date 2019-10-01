454a547e50f2e379ff8fb33d7d73e0d627dc2735
Ali Karimi <ajkarimi7@gmail.com>
2019-10-01 19:57:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,10 @@
 class Transfer
   # your code here
   
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
   
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender 
 end
